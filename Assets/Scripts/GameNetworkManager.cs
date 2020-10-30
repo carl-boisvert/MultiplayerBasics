9fdd9662097cdbc3a47595b569e9af4dc9b9f22c
Snappydue <cboisvert@egn.tv>
2020-10-30 07:45:35 -0400

    First Commmit

---
+++ Assets/Scripts/GameNetworkManager.cs
@@ -0,0 +1,18 @@
+﻿using Mirror;
+using UnityEngine;
+
+public class GameNetworkManager : NetworkManager
+{
+    public override void OnServerAddPlayer(NetworkConnection conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        NetworkPlayer player = conn.identity.gameObject.GetComponent<NetworkPlayer>();
+        player.SetPlayerName($"Player {numPlayers}");
+        player.SetPlayerColor(new Color(
+            Random.Range(0f, 1f), 
+            Random.Range(0f, 1f), 
+            Random.Range(0f, 1f)
+        ));
+    }
+}
---
+++ Assets/Scripts/NetworkPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Mirror;
+using UnityEngine;
+
+public class NetworkPlayer : NetworkBehaviour
+{
+    [SyncVar][SerializeField] private String _playerName;
+    [SyncVar][SerializeField] private Color _playerColor;
+
+    [Server]
+    public void SetPlayerName(String playerName)
+    {
+        _playerName = playerName;
+    }
+    
+    public void SetPlayerColor(Color playerColor)
+    {
+        _playerColor = playerColor;
+    }
+}
